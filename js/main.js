bfd0459bc3a191be85ce3fe4071eca7a44499a82
Lazar-crypto <lazaj1999@gmail.com>
2020-12-21 13:48:37 +0100

    drugi commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 
+var width = document.getElementById("img1").width ;
+var height = document.getElementById("img1").height ;
+var newwidth = document.getElementById("img1").width * 1.1;
+var newheight = document.getElementById("img1").height * 1.1;
+
+console.log(width, height , newheight , newwidth)
+
+
 $(document).ready(function(){
+  
     $("#img1").mouseenter(function(){
+        document.getElementById("img1").style.maxWidth = "110%";
         $(this).css("cursor","pointer");
-        $(this).animate({width: "319px", height: "319px"}, 500);
+        $(this).animate({width: newwidth , height: newheight}, 500);
     });
     $("#img1").mouseleave(function(){
         
-        $(this).animate({width: "290px", height: "290px"}, 500);
+        $(this).animate({width: width, height: height}, 500);
     });
     
 });
